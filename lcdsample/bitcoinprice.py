5a405a7f1e882680f0a512c31e56b33ccf54e6a4
Will Sheehan <willsheehan95@gmail.com>
2017-05-08 19:57:25 +0000

    new lcd example

---
+++ lcdsample/bitcoinprice.py
@@ -0,0 +1,39 @@
+import Adafruit_CharLCD as LCD
+from subprocess import *
+from datetime import datetime
+import requests
+import json
+from time import sleep
+
+# Initialize the LCD using the pins
+lcd = LCD.Adafruit_CharLCDPlate()
+
+# Wipe whatever was previously on the screen and load our message
+lcd.clear()
+lcd.message('S:BTC,')
+lcd.message('R:ETH')
+
+json_body = requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin').text
+body = json.loads(json_body)
+json_body1 = requests.get('https://api.coinmarketcap.com/v1/ticker/ethereum').text
+body1 = json.loads(json_body1)
+
+# Initialize counter
+counter = 0
+
+while counter < 5:
+    if lcd.is_pressed(LCD.SELECT):
+        lcd.clear()
+        lcd.message('BTC USD:') 
+        lcd.message(body[0]['price_usd'])
+        sleep(1.0)
+        counter+=1
+    if lcd.is_pressed(LCD.RIGHT):
+        lcd.clear()
+        lcd.message('ETH USD:')
+        lcd.message(body1[0]['price_usd'])
+        sleep(1.0)
+        counter+=1
+        
+lcd.clear()
+lcd.message('Give new py file...')
