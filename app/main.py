38500d811f3f69519baf385dcbfa5a5e3782848d
Will Sheehan <wsheehan@bates.edu>
2017-05-08 15:46:19 -0400

    Coins object with our data of interest

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 import requests
 import json
 
-json_body = requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin').text
-body = json.loads(json_body)
+# Our current coins of interest
+coins = {
+	'monero': None,
+	'ethereum': None,
+	'litecoin': None
+}
 
-print(body[0]['id'])
+# Fetch coin data
+uri = 'https://api.coinmarketcap.com/v1/ticker/'
+def get_coin_data(coin):
+	j = requests.get(uri + coin).text
+	return json.loads(j)[0]
+
+# Update coin data from api
+for k, v in coins.items():
+	coins[k] = get_coin_data(k)
+
+print(coins)
