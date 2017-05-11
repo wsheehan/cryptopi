769cfd353ee7ba2681a816bb98dd01cb629fcbf9
Will Sheehan <willsheehan95@gmail.com>
2017-05-11 19:20:31 +0000

    functional csv text file

--- tasks/writeprice.py
+++ tasks/writeprice.py
@@ -4,7 +4,7 @@ import json
 json_body = requests.get('https://api.coinmarketcap.com/v1/ticker/monero').text
 body = json.loads(json_body)
 
-f=open('moneroprice.csv','a')
+f=open('/home/pi/cryptopi/tasks/moneroprice.csv','a')
 f.write('\n')
 f.write(body[0]['price_usd'])
 f.close()
